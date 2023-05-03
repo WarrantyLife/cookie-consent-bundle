9ead1dcacca57fd9044507a358430fd7c13ac518
Scott Brown <scott@liangbrown.com>
2023-05-03 12:39:58 -0700

    Symfony 3.4 support
    Use Symfony\Component\Translation\TranslatorInterface instead of
    Symfony\Contracts\Translation\TranslatorInterface.

--- Controller/CookieConsentController.php
+++ Controller/CookieConsentController.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 use Twig\Environment;
 
 class CookieConsentController
